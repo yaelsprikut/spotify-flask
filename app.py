db5152aae2677d0d7931996ed93dc21dc31a32ec
yaelsprikut <ysprikut@gmail.com>
2022-08-28 19:14:53 -0400

    add script method to run worker

--- app.py
+++ app.py
@@ -7,9 +7,11 @@
 '''
 
 import json
+import sys
 import threading
 from flask import Flask, request, redirect, g, render_template, session
 from spotify_requests import spotify
+from youtube_search import YoutubeSearch
 app = Flask(__name__)
 app.secret_key = 'some key for session'
 
@@ -38,6 +40,32 @@ def valid_token(resp):
 def index():
     return render_template('index.html')
 
+@app.route('/run-script')
+def run_script():
+    with open('data.txt', 'r', encoding='utf-8') as f:
+        last_total = f.read()
+    def get_track_list_len():
+        threading.Timer(10.0, get_track_list_len).start()
+        # # get user playlist data
+        playlist_data = spotify.get_users_playlist_tracks({'Authorization': 'Bearer BQBU6WLNHUcegLrL1hcTLAX7xPAi-CmXi0gsnGliglP7k8EdGcRPftSWN9RzhpZq7NIIG_88bOsC7XurN7Fgpn-crwDtEK6QJ6WBk-E9l5Ba2eHXHbsSY98eaneY943ZGoKVYQp2relbP7uhRUYWnVueE8jUoOI4eTLhdIn36YJ3GhSRtddDv-SNJw7x5ZkQ4n0HPX81ZYEpb-FTyw8xu0x1_p0UnvPqwcn6CAqN0VYwjqM3KYBXGgBw'}, "3E9kzE2uBv5CVPX089GWyw")
+        print(playlist_data)
+
+        total_tracks = int(playlist_data["total"])
+        if total_tracks == int(last_total):
+            print("loop back <<<")
+            return False
+        print("total_tracks: ", type(total_tracks), type(last_total))
+        results = YoutubeSearch("{} {}".format(playlist_data["items"][total_tracks - 1]["track"]["name"], playlist_data["items"][total_tracks - 1]["track"]["artists"][0]["name"]), max_results=10).to_dict()
+        if results:
+            print("results", results[0]["url_suffix"])
+
+
+        with open('data.txt', 'w', encoding='utf-8') as f:
+            json.dump(playlist_data["total"], f)
+        # print(playlist_data)
+ 
+    get_track_list_len()
+    return "Run script for worker"
 
 @app.route('/search/')
 def search():
@@ -96,18 +124,6 @@ def artist(id):
                            image_url=image_url)
 
 
-@app.route('/run-script')
-def run_script():
-    access_token = request.args.get("code")
-    def get_track_list_len():
-        threading.Timer(8.0, get_track_list_len).start()
-        # # get user playlist data
-        playlist_data = spotify.get_users_playlist_tracks({'Authorization': 'Bearer BQD7HVMm09TrOlqoaxzNrG4rqd6MJY-yfBSs8E_UGUnF5wE2cbDCDdZCGNJx93mg5vfxZzOjdasYte6PxNgT66jXGVQW2tzA8F9uAR0CAp9SKE4yuPohN_RdxDhrn9mrNU1zzUG97nqAV1IuaZjLfxx2cbJE7AiLiRm4JxxluRXaNQGHw8Uv_-i5ZD0nkf3psAaTDX8immBR3ZwwrSUgWSPTQKH8TUZ2urz3cnZi4OXu12PPKPuny58L'}, "3E9kzE2uBv5CVPX089GWyw")
-        print("playlist_data: ", playlist_data["total"])
-        # print(playlist_data)
- 
-    get_track_list_len()
-    return "Run script for worker"
 
 
 @app.route('/profile')
